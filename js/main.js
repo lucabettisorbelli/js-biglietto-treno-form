21d8377996720d7aa9adb5be4e1774ba5fe944f2
luca betti sorbelli <luca betti sorbelli>
2023-04-14 18:11:06 +0200

    bozza

--- js/main.js
+++ js/main.js
@@ -1,50 +1,44 @@
-// let etaUtente = parseInt(prompt("Inserisci la tua età"));
-// let distance = parseInt(prompt("Inserisci la distanza chilometrica"));
 
-bottone.addEventListener("click", function() {
+bottone.addEventListener("click", function () {
+
+    //dati distanza in km
 
     let distance = document.getElementById("chilometri").value;
     distance = parseInt(distance);
+    console.log(`la distanza in km è di: ${distance}`);
 
-    let etaUtente = document.getElementById("eta").value;
-    etaUtente = parseInt(etaUtente)
+    //dati età utente
     
+    let etaUtente = document.getElementById("eta").value;
+    etaUtente = parseInt(etaUtente);
+    console.log(`l'età utente è di: ${etaUtente}`);
+
     if (isNaN(distance)) {
         console.log("La distanza non è un numero. Impossibile procedere.");
     } else {
-
         let prezzo = distance * 0.21;
         let messaggio = "";
-    
         console.log("Prezzo di base", prezzo);
-    }
 
-    if (isNaN(etaUtente)) {
-        alert("L'età è errata. Non verranno applicate scontistiche");
-    } else {
-
-        if (etaUtente < 18) {
-            prezzo *= 0.8;
-
-            messaggio = `Hai diritto allo sconto 'under 18'.`;
-
-        } else if (etaUtente > 65) {
-            // console.log("L'utente è over 65");
-            prezzo *= 0.55;
-
-            messaggio = `Hai diritto allo sconto 'senior'.`;
+        if (isNaN(etaUtente)) {
+            console.log("L'età è errata. Non verranno applicate scontistiche");
+        } else {
+            if (etaUtente < 18) {
+                prezzo = prezzo * 0.8;
+                messaggio = `Hai diritto allo sconto 'under 18'.`;
+            } else if (etaUtente > 65) {
+                prezzo = prezzo * 0.55;
+                messaggio = `Hai diritto allo sconto 'senior'.`;
+            }
         }
-    }
 
-    console.log(distance);
+        messaggio += ` Il prezzo del biglietto è: ${prezzo.toFixed(2)} €`;
+        document.getElementById("risultato").innerText = messaggio;
+        console.log("Prezzo finale", prezzo);
+    }
 });
 
 
-    messaggio += ` Il prezzo del biglietto è: ${prezzo.toFixed(2)} €`;
-
-    document.getElementById("risultato").innerText = messaggio;
-
-    console.log("Prezzo finale", prezzo);
 
 
 
